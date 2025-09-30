ff321dd40e41a7405eb9f114c7c73823f37a3747
CR0NYM3X <88253386+CR0NYM3X@users.noreply.github.com>
2025-09-30 12:30:30 -0700

    Create 01_variables_tipos_operadores.py

---
+++ 01_variables_tipos_operadores.py
@@ -0,0 +1,42 @@
+
+# En Python no necesitas declarar el tipo de variable, se asigna automáticamente.
+nombre = "Juan"
+edad = 30
+altura = 1.75
+
+''' Comentarios de varias lineas '''
+
+# 2. Tipos de datos
+numero = 42          # int
+precio = 19.99       # float
+mensaje = "Hola"     # str
+activo = True        # bool
+colores = ["rojo", "verde", "azul"]  # list
+usuario = {"nombre": "Ana", "edad": 28}  # dict
+
+
+# 3. Operadores 
+
+##      Aritméticos
+a = 10
+b = 3
+print(a + b)  # Suma → 13
+print(a - b)  # Resta → 7
+print(a * b)  # Multiplicación → 30
+print(a / b)  # División → 3.333...
+print(a % b)  # Módulo → 1
+print(a ** b) # Potencia → 1000
+
+##      Comparación
+print(a == b)  # Igualdad → False
+print(a != b)  # Diferente → True
+print(a > b)   # Mayor → True
+print(a < b)   # Menor → False
+
+
+##      Lógicos
+x = True
+y = False
+print(x and y)  # AND → False
+print(x or y)   # OR → True
+print(not x)    # NOT → False
